c6cc7b6b23f09be40ced71eebb6e2e33846e1460
Gabriel <ghbranco6@gmail.com>
2024-10-17 22:25:17 -0300

    base fetching and general code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState, useEffect  } from 'react'
+import { useState } from 'react';
+import { fetchOptions, BASE_URL } from './api/api.js';
 
 function App() {
+    const [searchedWord, setSearchedWord] = useState('');
+    const [fetchOption, setFetchOption] = useState('');
+    const [response, setResponse] = useState(undefined);
 
-  const BASE_URL = 'https://api.datamuse.com/'
-  const test = 'words?sl=car'
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-  const fetchData = async(url) => {
-    if (typeof url !== 'string') return;
+    const fetchData = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
 
-    const res = await fetch(BASE_URL + url)
-    const json = await res.json()
-    console.log(json)
-  }
+        if (searchedWord.length < 1) alert('Please type a longer word');
 
-  // useEffect(() => {
-  //   fetchData(test)
-  // }, [])
+        try {
+            const res = await fetch(
+                `${BASE_URL}/words?${fetchOption}=${searchedWord}`,
+            );
+            const json = await res.json();
+            setResponse(json);
+            console.log(json);
 
-  return (
-    <>
-      <div>
-      </div>
-      <h1 className='font-sans text-4xl'>Vite + React</h1>
-      <div >
-        <button className='bg-zinc-200 p-3 rounded-xl border hover:bg-slate-500 transition-all font-mono' onClick={() => fetchData(test)}>
-          Fetch <br /> {test}
-        </button>
-      </div>
-    </>
-  )
+            if (json.length === 0) {
+                alert('Word not found.\n Please try again');
+            }
+        } catch (e) {
+            console.error(`Erro: ${e}`);
+            setError(true);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <section className="p-5 max-w-[1200px] my-0 mx-auto min-h-screen flex flex-col items-center">
+            <div>
+                <h1 className="font-sans text-4xl">Datamuse API</h1>
+                <p> A word-finding query engine </p>
+            </div>
+            <form onSubmit={(e) => fetchData(e)}>
+                <input
+                    value={searchedWord}
+                    className="bg-gray-500 rounded-none"
+                    type="text"
+                    onChange={({ target }) => setSearchedWord(target.value)}
+                />
+                <p>Word State:{searchedWord}</p>
+
+                {Object.values(fetchOptions).map((option) => (
+                    <div key={option.key}>
+                        <input
+                            type="radio"
+                            value={option.key}
+                            name="fetchOptions"
+                            id={option.key}
+                            onChange={({ target }) =>
+                                setFetchOption(target.value)
+                            }
+                            checked={fetchOption === option.key}
+                        />
+                        <label htmlFor={option.key}>{option.description}</label>
+                    </div>
+                ))}
+
+                <p>Selected Radio Input = {fetchOption}</p>
+                <button
+                    type="submit"
+                    disabled={isLoading || error}
+                    className="bg-zinc-200 p-3 rounded-xl border hover:bg-slate-500 transition-all font-mono disabled:cursor-default disabled:bg-red-400"
+                >
+                    {isLoading ? 'Loading...' : 'Fetch Data'}
+                </button>
+            </form>
+            {response && (
+                <ul>
+                    {response.map((res, idx) => (
+                        <li key={idx}>{res.word}</li>
+                    ))}
+                </ul>
+            )}
+        </section>
+    );
 }
 
-export default App
+export default App;
